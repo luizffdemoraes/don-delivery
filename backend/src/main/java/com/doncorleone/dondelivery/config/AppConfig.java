c368fb0f7e3805338f9eb2cb3b4ca9c1ffaf0c3e
Luiz Moraes <lffm1994@gmail.com>
2021-09-15 04:39:02 -0300

    Adição de validação de endpoint e login

--- backend/src/main/java/com/doncorleone/dondelivery/config/AppConfig.java
+++ backend/src/main/java/com/doncorleone/dondelivery/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.doncorleone.dondelivery.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean //componente do Spring que vai gerenciar o componente. Como se fosse o @service, porém para metodos
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -17,7 +21,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
--- backend/src/main/java/com/doncorleone/dondelivery/config/AthorizationServerConfig.java
+++ backend/src/main/java/com/doncorleone/dondelivery/config/AthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.doncorleone.dondelivery.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${jwt.duration}")
+    private Integer jwtDuration;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -36,11 +46,11 @@ public class AthorizationServerConfig extends AuthorizationServerConfigurerAdapt
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("don-delivery")
-                .secret(passwordEncoder.encode("don-delivery123"))
+                .withClient(clientId)
+                .secret(passwordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(86400); // 24 horas
+                .accessTokenValiditySeconds(jwtDuration); // 24 horas
     }
 
     @Override
